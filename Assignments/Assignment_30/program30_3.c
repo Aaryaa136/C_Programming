f24d44e35a940d2fed6e35f7d35c860d6a5d3beb
Aaryaa136 <patilaaryaa85@gmail.com>
2025-11-25 13:14:46 +0530

    Add files via upload

---
+++ Assignments/Assignment_30/program30_3.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////
+//
+//Function Name:    Pattern
+//Description:      Display non linear pattern
+//Input:            Integer
+//Author:           Aaryaa Patil
+//Date:             25/11/25
+//
+//////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i==j)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &iValue2);
+
+    if(iValue1 != iValue2)
+    {
+        printf("Invalid input....\n");
+        printf("Enter same number of rows and columns");
+        return 0;
+    }
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
